52a6bfa9a5c6f76a96f95601c94d69c007ea727e
MrSuraj1 <surajyadavsy271@gmail.com>
2025-11-27 17:43:32 +0530

    Initial commit

--- Routes/token.js
+++ Routes/token.js
@@ -6,22 +6,25 @@ const router = express.Router();
 router.get("/", (req, res) => {
   try {
     const API_KEY = process.env.VIDEOSDK_API_KEY;
-    const SECRET = process.env.VIDEOSDK_SECRET;
+    const SECRET = process.env.VIDEOSDK_SECRET; // ✔️ Correct One
 
     if (!API_KEY || !SECRET) {
-      return res.status(500).json({ error: "API Key or Secret missing" });
+      return res.status(500).json({ error: "Missing API_KEY or SECRET" });
     }
 
     const payload = {
       apikey: API_KEY,
-      permissions: [
-        "allow_join",
-        "allow_mod",
-        "allow_create",
-        "allow_webcam",
-        "allow_screen_share"
-      ],
       version: 2,
+      permissions: [
+       "allow_join",
+        "allow_mod",
+        "allow_webcam",
+        "allow_mic",
+        "allow_screen_share",
+        "allow_chat",
+        "allow_create",
+        "allow_recording",
+        "allow_server_config"]
     };
 
     const token = jwt.sign(payload, SECRET, {
@@ -31,6 +34,7 @@ router.get("/", (req, res) => {
 
     res.json({ token });
   } catch (err) {
+    console.error("Token error:", err);
     res.status(500).json({ error: "Token generation failed" });
   }
 });
